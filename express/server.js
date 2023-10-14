62c25e11dd39840f2c839829ce10a2d37c0b9965
sandip388 <tarsariyasandip5@gmail.com>
2023-10-15 00:13:21 +0530

    small changes

--- express/server.js
+++ express/server.js
@@ -436,7 +436,7 @@ router.get("/search_movie/:name", async (req, res) => {
 //     console.log("running",9000)
 // });
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
